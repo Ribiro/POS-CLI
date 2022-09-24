2c64e45be2ee45abbe2d07e4110d77e0033ef314
Ribiro <deno059861>
2022-09-25 00:13:35 +0300

    set project structure

---
+++ Controllers/menu.py
@@ -0,0 +1,12 @@
+def menu():
+    print('***Welcome to POS CLI***')
+    
+    # these print statements provides the user with the available options to choose from
+    print('1. Customer Operations')
+    print('2. Product Operations')
+    print('3. Purchase Operations')
+    print('0. Quit')
+
+    # take user choice
+    user_choice = input('Please enter your choice to proceed: ')
+    pass
---
+++ main.py
@@ -0,0 +1,12 @@
+from Controllers.menu import menu
+
+# this main function calls the menu function
+def main():
+    menu()
+
+
+
+
+# this part calls the main function when this file is run
+if __name__ == '__main__':
+    main()
